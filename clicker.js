0bbdda3a6fddf3388e60ea4d59d47828b34f911e
Smith06S <sarahelenasmith@.com>
2025-06-01 18:41:06 +0200

    to do list

--- clicker.js
+++ clicker.js
@@ -1,7 +1,18 @@
 let count = 0;
 
-
 setInterval(() => {
   console.log(count);
   count++;
 }, 1000); 
+
+
+// conto di liv 1 (primo con 200 poi + 100)
+// conto di liv 2 (primo 500 + 200)
+// conto di liv 3 (primo 1500 + 500)
+// questi si aggiungono 1+2+3
+// conto quando cliccki 
+
+/*
+when arrives to maximum supported, am i supposed to put infinity to the count?
+
+*/
